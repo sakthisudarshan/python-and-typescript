741e0c1eeaf20505316dc4ba2f6e1055af3551d8
Ayush Raj <ayush.raj@mercedes-benz.com>
2026-07-26 01:34:29 +0530

    fix(anomaly-detection): added test issed out in imports Signed-off-by: Ayush Raj  <ayush.raj@mercedes-benz.com>

--- superset-frontend/src/dashboard/components/nativeFilters/FilterBar/Vertical.tsx
+++ superset-frontend/src/dashboard/components/nativeFilters/FilterBar/Vertical.tsx
@@ -45,6 +45,7 @@ import Header from './Header';
 import FilterControls from './FilterControls/FilterControls';
 import CrossFiltersVertical from './CrossFilters/Vertical';
 import crossFiltersSelector from './CrossFilters/selectors';
+import UrlFiltersVertical from './UrlFilters/Vertical';
 
 enum SectionType {
   Filters = 'filters',
@@ -300,6 +301,7 @@ const VerticalFilterBar: FC<VerticalBarProps> = ({
           ) : (
             <div css={tabPaneStyle} onScroll={onScroll}>
               <>
+                <UrlFiltersVertical />
                 <CrossFiltersVertical hideHeader={hasOnlyOneSectionType} />
                 {filterControls}
               </>
